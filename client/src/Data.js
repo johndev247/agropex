beaf17a5d9449973833eade638a48337016c1f22
johndev247 <johndev247@gmail.com>
2021-08-24 00:09:41 +0100

    client

---
+++ client/src/Data.js
@@ -0,0 +1,154 @@
+export const tickets = [
+  {
+    id: 1,
+    user: "John",
+    title: "SSL ISSUE",
+    desc: "Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.",
+    status: "Opened",
+    date: new Date().toLocaleString(),
+    msghistory: {
+      messages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+      adminmessages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+    },
+  },
+  {
+    id: 2,
+    user: "Simon",
+    title: "Profile Picture",
+    desc: "Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.",
+    status: "Opened",
+    date: new Date().toLocaleString(),
+    msghistory: {
+      messages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+      adminmessages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+    },
+  },
+  {
+    id: 3,
+    user: "Peter",
+    title: "Dashboard",
+    status: "Closed",
+    desc: "Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.",
+    date: new Date().toLocaleString(),
+    msghistory: {
+      messages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+      adminmessages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+    },
+  },
+  {
+    id: 4,
+    user: "Blessing",
+    title: "Profile Update",
+    desc: "Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Cras ultricies ligula sed magna dictum porta.",
+    status: "Opened",
+    date: new Date().toLocaleString(),
+    msghistory: {
+      messages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+      adminmessages: [
+        {
+          id: 1,
+          date: "23-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+        {
+          id: 2,
+          date: "27-1-21",
+          message:
+            "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus.",
+        },
+      ],
+    },
+  },
+];
