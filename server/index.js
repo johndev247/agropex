1fe8db692d154897ab7341b19bd1b48ca9927ebc
johndev247 <johndev247@gmail.com>
2021-08-24 01:22:20 +0100

    setting up

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const {ApolloServer} = require("apollo-server");
 const typeDefs = require("./src/graphql/typeDefs");
 const resolvers = require("./src/graphql/resolvers/index");
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 const Db = process.env.DB;
 
 const server = new ApolloServer({
@@ -18,7 +18,10 @@ mongoose
   })
   .then(async () => {
     console.log("Mongodb Connected");
-    return server.listen({port}).then((res) => {
-      console.log(`Sever running on ${res.url}`);
-    });
+    return server
+      .listen({port})
+      .then((res) => {
+        console.log(`Sever running on ${res.url}`);
+      })
+      .catch((err) => console.log(err));
   });
